d3e925f9980c8c9df4563ddbdd71b78cdd8340e3
Iago <iagofreits@gmail.com>
2024-11-23 22:29:08 -0300

    novo ingrediente

--- potionseller/scripts/caldeirao.gd
+++ potionseller/scripts/caldeirao.gd
@@ -1,5 +1,4 @@
 extends Node2D
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("Um corpo entrou: ", body.name)
-	body.queue_free()
+func _on_area_2d_body_entered(body: Node2D):
+		queue_free()

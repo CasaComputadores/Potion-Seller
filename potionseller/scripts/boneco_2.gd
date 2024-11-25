56bd02ee2eb3f0b8c48df184adb1c70f2187ed4f
Marcos Windows <marcosmalta132@gmail.com>
2024-11-25 14:03:36 -0300

    Mais um commite

--- potionseller/scripts/boneco_2.gd
+++ potionseller/scripts/boneco_2.gd
@@ -1,9 +1,16 @@
 extends Node2D
-
+#var coin = 0;
+@onready var hud = $HUD/Moeda
+var contadorMoeda: int = 0
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	print("Objeto entrou:", area.name)
 	if area.is_in_group("pocoes"):
 		var parent = area.get_parent()
 		if parent:
+			coletarMoeda()
 			parent.queue_free()
 			print("Poção Vendida:", parent.name)
+func coletarMoeda():
+	contadorMoeda +=1
+	hud.text = "Moedas: %d" % contadorMoeda
+	

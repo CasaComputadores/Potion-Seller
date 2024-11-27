2da7436a206e595cf1838cf857ff0b842b14381b
Iago <iagofreits@gmail.com>
2024-11-27 10:20:29 -0300

    funcionalidades da musica + arquivos de efeitos sonoros

---
+++ potionseller/scripts/menu.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+func _ready() -> void:
+	# Configura o volume inicial com base no Autoload
+	MusicaMenu.set_volume()
--- potionseller/scripts/opcoes.gd
+++ potionseller/scripts/opcoes.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-var master_bus = AudioServer.get_bus_index("Master")
+func _ready() -> void:
+	# Obtém o slider e ajusta seu valor com base no volume global armazenado
+	var slider = $VBoxContainer/VolumeMusica  # Substitua "Slider" pelo caminho correto
+	slider.value = MusicaMenu.volume_value  # Usa o valor global do Autoload
+	MusicaMenu.set_volume()  # Aplica o volume ao iniciar a cena de opções
 
 func _on_volume_musica_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(master_bus, value)
-	
-	if value == -30:
-		AudioServer.set_bus_mute(master_bus, true)
-	else:
-		AudioServer.set_bus_mute(master_bus, false)
+	# Atualiza o volume global e aplica as mudanças
+	MusicaMenu.volume_value = value
+	MusicaMenu.set_volume()

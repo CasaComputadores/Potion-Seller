a42f6705be7e6d0116834f9190ce63c66e3fc8b5
Iago <iagofreits@gmail.com>
2024-11-28 11:35:07 -0300

    moeda aparecer na mesa

--- potionseller/sprites/area_2d.gd
+++ potionseller/sprites/area_2d.gd
@@ -5,11 +5,10 @@ func _physics_process(delta: float) -> void:
 	if selected:
 		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
 
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		deselect_all() # Deselect all other items
 		selected = true # Select this item
-
 
 # Desseleciona todos os itens no grupo
 func deselect_all() -> void:
@@ -27,10 +26,3 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
 			selected = false
-
-
-
-
-
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	pass # Replace with function body.
